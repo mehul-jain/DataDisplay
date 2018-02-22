d25691faf8b1bc28f6baf6a0c9b5ea381cf8ab45
mehul-jain <mehul@infrrd.ai>
2018-02-22 15:05:13 +0530

    added callback from async task class

---
+++ app/src/main/java/com/example/admin/showdata/DownloadTask.java
@@ -0,0 +1,36 @@
+package com.example.admin.showdata;
+
+import android.os.AsyncTask;
+import java.util.List;
+
+/**
+ * Created by admin on 22-02-2018.
+ */
+
+public class DownloadTask extends AsyncTask<String, Integer, List<ContentModel>> {
+
+    private final TaskListener taskListener;
+
+    public DownloadTask(TaskListener taskListener) {
+        this.taskListener = taskListener;
+    }
+
+    @Override
+    protected void onPreExecute() {
+//        super.onPreExecute();
+        taskListener.onTaskStarted();
+    }
+
+    @Override
+    protected void onPostExecute(List<ContentModel> content_models) {
+//        super.onPostExecute(content_models);
+        taskListener.onTaskFinished(content_models);
+    }
+
+    @Override
+    protected List<ContentModel> doInBackground(String... url) {
+        DownloadData downloadData = new DownloadData();
+        List<ContentModel> data = downloadData.getdatafromurl(url[0]);
+        return data;
+    }
+}
--- app/src/main/java/com/example/admin/showdata/MainActivity.java
+++ app/src/main/java/com/example/admin/showdata/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.admin.showdata;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,54 +8,40 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TaskListener {
+    private ContentLoadingProgressBar contentLoadingProgressBar;
+
+    @Override
+    public void onTaskStarted() {
+        contentLoadingProgressBar = (ContentLoadingProgressBar) findViewById(R.id.loader);
+        contentLoadingProgressBar.show();
+    }
+
+    @Override
+    public void onTaskFinished(List<ContentModel> list) {
+        if(contentLoadingProgressBar!=null)
+            contentLoadingProgressBar.hide();
+        Log.d("fetched data", "" + list);
+        if(list !=null && !list.isEmpty()) {
+            // setting Recycler View
+            RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+            RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(list, getApplication());
+            recyclerView.setAdapter(recyclerViewAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // getting data from the api
-        new DownloadTask().execute("http://www.mocky.io/v2/5a85f4a33100006800253180");
+        new DownloadTask(this).execute("http://www.mocky.io/v2/5a85f4a33100006800253180");
+
     }
-
-    private class DownloadTask extends AsyncTask<String,Integer,List<ContentModel>>
-    {
-        private ContentLoadingProgressBar contentLoadingProgressBar;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            contentLoadingProgressBar=  (ContentLoadingProgressBar) findViewById(R.id.loader);
-               contentLoadingProgressBar.show();
-        }
-
-        @Override
-        protected void onPostExecute(List<ContentModel> content_models) {
-            contentLoadingProgressBar.hide();
-            super.onPostExecute(content_models);
-            Log.d("fetched data",""+content_models);
-
-            // setting Recycler View
-            RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recycler_view);
-            RecyclerViewAdapter recycler_view_adapter= new RecyclerViewAdapter(content_models,getApplication());
-            recyclerView.setAdapter(recycler_view_adapter);
-            recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        }
-
-        @Override
-        protected List<ContentModel> doInBackground(String... url) {
-            DownloadData downloadData = new DownloadData();
-            List<ContentModel> data= downloadData.getdatafromurl(url[0]);
-            return data;
-        }
-    }
-
-    public void sendMessage(View view)
-    {
+    public void sendMessage(View view) {
         Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
         startActivity(intent);
     }
